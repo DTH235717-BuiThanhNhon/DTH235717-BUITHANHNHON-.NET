d27db60b0a1da58c0292242a31a510a7a3f92a54
BUITHANHNHON <nhon_dth235717@student.agu.edu.vn>
2025-09-26 09:42:39 +0700

    Bài Tập

---
+++ BuoiTH3/Bai3b/Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Bai3b
+{
+    public partial class Form1 : Form  
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void lstWeb_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.lstWeb.Items.Add("Tuổi trẻ");
+            this.lstWeb.Items.Add("Thanh niên");
+            this.lstWeb.Items.Add("VNExpress");
+            this.lstWeb.Items.Add("Dân trí");
+            this.lstWeb.Items.Add("Công an");
+            this.lstWeb.SelectedItem = "Tuổi trẻ";
+        }
+
+        private void btnOK_Click(object sender, EventArgs e)
+        {
+            this.txtKq.Text = "Bạn đã chọn website ";
+            this.txtKq.Text += this.lstWeb.SelectedItem.ToString();
+        }
+
+        private void btnReset_Click(object sender, EventArgs e)
+        {
+            this.txtKq.Clear();
+        }
+    }
+}
