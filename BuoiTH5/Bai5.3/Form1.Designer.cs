b22c4fe0f2faf4377c5c60f01eab353c2f1cc2e2
BUITHANHNHON <nhon_dth235717@student.agu.edu.vn>
2025-10-02 15:09:34 +0700

    Bài Tập

---
+++ BuoiTH5/Bai5.3/Form1.Designer.cs
@@ -0,0 +1,334 @@
+﻿namespace Bai5._3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.label12 = new System.Windows.Forms.Label();
+            this.btnCong = new System.Windows.Forms.Button();
+            this.btnTru = new System.Windows.Forms.Button();
+            this.btnNhan = new System.Windows.Forms.Button();
+            this.btnChia = new System.Windows.Forms.Button();
+            this.btnLamLai = new System.Windows.Forms.Button();
+            this.btnThoat = new System.Windows.Forms.Button();
+            this.txtTu1 = new System.Windows.Forms.TextBox();
+            this.txtMau1 = new System.Windows.Forms.TextBox();
+            this.txtTu2 = new System.Windows.Forms.TextBox();
+            this.txtMau2 = new System.Windows.Forms.TextBox();
+            this.txtMauKQ = new System.Windows.Forms.TextBox();
+            this.txtTuKQ = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Red;
+            this.label1.Location = new System.Drawing.Point(255, 43);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(260, 32);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Phép Tính Phân Số";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(113, 119);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(69, 16);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Phân số 1:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(407, 119);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(69, 16);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Phân số 2:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(113, 230);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(68, 16);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Phép Tính";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(391, 230);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(85, 16);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Kết quả cộng";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(136, 149);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(46, 16);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "Tử Số:";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(126, 174);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(56, 16);
+            this.label7.TabIndex = 6;
+            this.label7.Text = "Mẫu Số:";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(228, 358);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(0, 16);
+            this.label8.TabIndex = 7;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(431, 149);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(46, 16);
+            this.label9.TabIndex = 8;
+            this.label9.Text = "Tử Số:";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(420, 180);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(56, 16);
+            this.label10.TabIndex = 9;
+            this.label10.Text = "Mẫu Số:";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(430, 257);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(46, 16);
+            this.label11.TabIndex = 10;
+            this.label11.Text = "Tử Số:";
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Location = new System.Drawing.Point(420, 288);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(56, 16);
+            this.label12.TabIndex = 11;
+            this.label12.Text = "Mẫu Số:";
+            // 
+            // btnCong
+            // 
+            this.btnCong.Location = new System.Drawing.Point(139, 249);
+            this.btnCong.Name = "btnCong";
+            this.btnCong.Size = new System.Drawing.Size(100, 37);
+            this.btnCong.TabIndex = 12;
+            this.btnCong.Text = "Cộng";
+            this.btnCong.UseVisualStyleBackColor = true;
+            this.btnCong.Click += new System.EventHandler(this.btnCong_Click_1);
+            // 
+            // btnTru
+            // 
+            this.btnTru.Location = new System.Drawing.Point(261, 247);
+            this.btnTru.Name = "btnTru";
+            this.btnTru.Size = new System.Drawing.Size(99, 37);
+            this.btnTru.TabIndex = 13;
+            this.btnTru.Text = "Trừ";
+            this.btnTru.UseVisualStyleBackColor = true;
+            this.btnTru.Click += new System.EventHandler(this.btnTru_Click);
+            // 
+            // btnNhan
+            // 
+            this.btnNhan.Location = new System.Drawing.Point(139, 292);
+            this.btnNhan.Name = "btnNhan";
+            this.btnNhan.Size = new System.Drawing.Size(100, 37);
+            this.btnNhan.TabIndex = 14;
+            this.btnNhan.Text = "Nhân";
+            this.btnNhan.UseVisualStyleBackColor = true;
+            this.btnNhan.Click += new System.EventHandler(this.btnNhan_Click);
+            // 
+            // btnChia
+            // 
+            this.btnChia.Location = new System.Drawing.Point(261, 290);
+            this.btnChia.Name = "btnChia";
+            this.btnChia.Size = new System.Drawing.Size(100, 37);
+            this.btnChia.TabIndex = 15;
+            this.btnChia.Text = "Chia";
+            this.btnChia.UseVisualStyleBackColor = true;
+            this.btnChia.Click += new System.EventHandler(this.btnChia_Click);
+            // 
+            // btnLamLai
+            // 
+            this.btnLamLai.Location = new System.Drawing.Point(289, 375);
+            this.btnLamLai.Name = "btnLamLai";
+            this.btnLamLai.Size = new System.Drawing.Size(100, 48);
+            this.btnLamLai.TabIndex = 16;
+            this.btnLamLai.Text = "Tiếp tục";
+            this.btnLamLai.UseVisualStyleBackColor = true;
+            this.btnLamLai.Click += new System.EventHandler(this.btnLamLai_Click);
+            // 
+            // btnThoat
+            // 
+            this.btnThoat.Location = new System.Drawing.Point(423, 375);
+            this.btnThoat.Name = "btnThoat";
+            this.btnThoat.Size = new System.Drawing.Size(100, 48);
+            this.btnThoat.TabIndex = 17;
+            this.btnThoat.Text = "Thoát";
+            this.btnThoat.UseVisualStyleBackColor = true;
+            this.btnThoat.Click += new System.EventHandler(this.btnThoat_Click);
+            // 
+            // txtTu1
+            // 
+            this.txtTu1.Location = new System.Drawing.Point(188, 146);
+            this.txtTu1.Name = "txtTu1";
+            this.txtTu1.Size = new System.Drawing.Size(175, 22);
+            this.txtTu1.TabIndex = 18;
+            // 
+            // txtMau1
+            // 
+            this.txtMau1.Location = new System.Drawing.Point(188, 174);
+            this.txtMau1.Name = "txtMau1";
+            this.txtMau1.Size = new System.Drawing.Size(175, 22);
+            this.txtMau1.TabIndex = 19;
+            // 
+            // txtTu2
+            // 
+            this.txtTu2.Location = new System.Drawing.Point(483, 143);
+            this.txtTu2.Name = "txtTu2";
+            this.txtTu2.Size = new System.Drawing.Size(180, 22);
+            this.txtTu2.TabIndex = 20;
+            // 
+            // txtMau2
+            // 
+            this.txtMau2.Location = new System.Drawing.Point(483, 177);
+            this.txtMau2.Name = "txtMau2";
+            this.txtMau2.Size = new System.Drawing.Size(180, 22);
+            this.txtMau2.TabIndex = 21;
+            // 
+            // txtMauKQ
+            // 
+            this.txtMauKQ.Location = new System.Drawing.Point(483, 282);
+            this.txtMauKQ.Name = "txtMauKQ";
+            this.txtMauKQ.Size = new System.Drawing.Size(180, 22);
+            this.txtMauKQ.TabIndex = 22;
+            // 
+            // txtTuKQ
+            // 
+            this.txtTuKQ.Location = new System.Drawing.Point(483, 251);
+            this.txtTuKQ.Name = "txtTuKQ";
+            this.txtTuKQ.Size = new System.Drawing.Size(180, 22);
+            this.txtTuKQ.TabIndex = 23;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txtTuKQ);
+            this.Controls.Add(this.txtMauKQ);
+            this.Controls.Add(this.txtMau2);
+            this.Controls.Add(this.txtTu2);
+            this.Controls.Add(this.txtMau1);
+            this.Controls.Add(this.txtTu1);
+            this.Controls.Add(this.btnThoat);
+            this.Controls.Add(this.btnLamLai);
+            this.Controls.Add(this.btnChia);
+            this.Controls.Add(this.btnNhan);
+            this.Controls.Add(this.btnTru);
+            this.Controls.Add(this.btnCong);
+            this.Controls.Add(this.label12);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Phân Số";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.Button btnCong;
+        private System.Windows.Forms.Button btnTru;
+        private System.Windows.Forms.Button btnNhan;
+        private System.Windows.Forms.Button btnChia;
+        private System.Windows.Forms.Button btnLamLai;
+        private System.Windows.Forms.Button btnThoat;
+        private System.Windows.Forms.TextBox txtTu1;
+        private System.Windows.Forms.TextBox txtMau1;
+        private System.Windows.Forms.TextBox txtTu2;
+        private System.Windows.Forms.TextBox txtMau2;
+        private System.Windows.Forms.TextBox txtMauKQ;
+        private System.Windows.Forms.TextBox txtTuKQ;
+    }
+}
+
---
+++ BuoiTH5/Bai5.3/Form1.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Bai5._3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private int UCLN(int a, int b)
+        {
+            a = Math.Abs(a); b = Math.Abs(b);
+            while (b != 0)
+            {
+                int t = a % b;
+                a = b;
+                b = t;
+            }
+            return a;
+        }
+
+        // Hàm rút gọn phân số
+        private void RutGon(ref int tu, ref int mau)
+        {
+            if (mau < 0) { tu = -tu; mau = -mau; }
+            int uc = UCLN(tu, mau);
+            tu /= uc;
+            mau /= uc;
+        }
+        private void HienKetQua(int tu, int mau)
+        {
+            RutGon(ref tu, ref mau);
+            txtTuKQ.Text = tu.ToString();
+            txtMauKQ.Text = mau.ToString();
+        }
+        private bool DocPhanSo(TextBox txtTu, TextBox txtMau, out int tu, out int mau)
+        {
+            tu = mau = 0;
+            if (!int.TryParse(txtTu.Text.Trim(), out tu))
+            {
+                MessageBox.Show("Tử số không hợp lệ!");
+                txtTu.Focus();
+                return false;
+            }
+            if (!int.TryParse(txtMau.Text.Trim(), out mau) || mau == 0)
+            {
+                MessageBox.Show("Mẫu số không hợp lệ (không được = 0)!");
+                txtMau.Focus();
+                return false;
+            }
+            return true;
+        }
+        private void btnCong_Click(object sender, EventArgs e)
+        {
+            if (!DocPhanSo(txtTu1, txtMau1, out int tu1, out int mau1)) return;
+            if (!DocPhanSo(txtTu2, txtMau2, out int tu2, out int mau2)) return;
+
+            int tu = tu1 * mau2 + tu2 * mau1;
+            int mau = mau1 * mau2;
+            HienKetQua(tu, mau);
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnCong_Click_1(object sender, EventArgs e)
+        {
+            if (!DocPhanSo(txtTu1, txtMau1, out int tu1, out int mau1)) return;
+            if (!DocPhanSo(txtTu2, txtMau2, out int tu2, out int mau2)) return;
+
+            int tu = tu1 * mau2 + tu2 * mau1;
+            int mau = mau1 * mau2;
+            HienKetQua(tu, mau);
+        }
+
+        private void btnTru_Click(object sender, EventArgs e)
+        {
+            if (!DocPhanSo(txtTu1, txtMau1, out int tu1, out int mau1)) return;
+            if (!DocPhanSo(txtTu2, txtMau2, out int tu2, out int mau2)) return;
+
+            int tu = tu1 * mau2 - tu2 * mau1;
+            int mau = mau1 * mau2;
+            HienKetQua(tu, mau);
+        }
+
+        private void btnNhan_Click(object sender, EventArgs e)
+        {
+            if (!DocPhanSo(txtTu1, txtMau1, out int tu1, out int mau1)) return;
+            if (!DocPhanSo(txtTu2, txtMau2, out int tu2, out int mau2)) return;
+
+            int tu = tu1 * tu2;
+            int mau = mau1 * mau2;
+            HienKetQua(tu, mau);
+        }
+
+        private void btnChia_Click(object sender, EventArgs e)
+        {
+            if (!DocPhanSo(txtTu1, txtMau1, out int tu1, out int mau1)) return;
+            if (!DocPhanSo(txtTu2, txtMau2, out int tu2, out int mau2)) return;
+            if (tu2 == 0)
+            {
+                MessageBox.Show("Không thể chia cho phân số bằng 0!");
+                return;
+            }
+
+            int tu = tu1 * mau2;
+            int mau = mau1 * tu2;
+            HienKetQua(tu, mau);
+        }
+
+        private void btnThoat_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btnLamLai_Click(object sender, EventArgs e)
+        {
+            txtTu1.Clear(); txtMau1.Clear();
+            txtTu2.Clear(); txtMau2.Clear();
+            txtTuKQ.Clear(); txtMauKQ.Clear();
+            txtTu1.Focus();
+        }
+    }
+}
