0b72057e1eec6f4027224b399d81c98955809948
BUITHANHNHON <nhon_dth235717@student.agu.edu.vn>
2025-10-02 14:49:04 +0700

    Bài Tập

---
+++ BuoiTH5/Bai5.2/Form1.Designer.cs
@@ -0,0 +1,112 @@
+﻿namespace Bai5._2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtNhapN = new System.Windows.Forms.TextBox();
+            this.btnKiemTra = new System.Windows.Forms.Button();
+            this.btnThoat = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Red;
+            this.label1.Location = new System.Drawing.Point(214, 64);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(290, 32);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Kiểm Tra Và Tìm SHH";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(162, 143);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(56, 16);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Nhập N:";
+            // 
+            // txtNhapN
+            // 
+            this.txtNhapN.Location = new System.Drawing.Point(236, 143);
+            this.txtNhapN.Name = "txtNhapN";
+            this.txtNhapN.Size = new System.Drawing.Size(234, 22);
+            this.txtNhapN.TabIndex = 2;
+            this.txtNhapN.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // btnKiemTra
+            // 
+            this.btnKiemTra.Location = new System.Drawing.Point(165, 205);
+            this.btnKiemTra.Name = "btnKiemTra";
+            this.btnKiemTra.Size = new System.Drawing.Size(139, 35);
+            this.btnKiemTra.TabIndex = 3;
+            this.btnKiemTra.Text = "Kiểm tra";
+            this.btnKiemTra.UseVisualStyleBackColor = true;
+            this.btnKiemTra.Click += new System.EventHandler(this.btnKiemTra_Click);
+            // 
+            // btnThoat
+            // 
+            this.btnThoat.Location = new System.Drawing.Point(331, 208);
+            this.btnThoat.Name = "btnThoat";
+            this.btnThoat.Size = new System.Drawing.Size(139, 35);
+            this.btnThoat.TabIndex = 4;
+            this.btnThoat.Text = "Thoat";
+            this.btnThoat.UseVisualStyleBackColor = true;
+            this.btnThoat.Click += new System.EventHandler(this.btnThoat_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnThoat);
+            this.Controls.Add(this.btnKiemTra);
+            this.Controls.Add(this.txtNhapN);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "KT SNT";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtNhapN;
+        private System.Windows.Forms.Button btnKiemTra;
+        private System.Windows.Forms.Button btnThoat;
+    }
+}
+
---
+++ BuoiTH5/Bai5.2/Form1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Bai5._2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private bool IsPerfect(int n)
+        {
+            int sum = 0;
+            for (int i = 1; i <= n / 2; i++)
+                if (n % i == 0) sum += i;
+            return sum == n;
+        }
+        private void btnKiemTra_Click(object sender, EventArgs e)
+        {
+            if (!int.TryParse(txtNhapN.Text.Trim(), out int n))
+            {
+                MessageBox.Show("Vui lòng nhập số nguyên hợp lệ!");
+                return;
+            }
+
+            if (IsPerfect(n))
+                MessageBox.Show($"{n} là số hoàn hảo");
+            else
+                MessageBox.Show($"{n} không phải số hoàn hảo");
+        }
+
+        private void btnThoat_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+    
+    }
+
